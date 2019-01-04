9966a240983619a49700ed525d1d570f4dbc76f6
dcordobag <32066181+dcordobag@users.noreply.github.com>
2019-01-04 18:20:31 -0500

    update

--- WPCamaraComercio/WPCamaraComercio/Views/FrmPaymentData.xaml.cs
+++ WPCamaraComercio/WPCamaraComercio/Views/FrmPaymentData.xaml.cs
@@ -25,5 +25,60 @@ namespace WPCamaraComercio.Views
             CmbTypeBuyer.SelectedIndex = 0;
             CmbIdDType.SelectedIndex = 0;
         }
+
+        private void BtnPay_StylusDown(object sender, StylusDownEventArgs e)
+        {
+
+        }
+
+        public void FillTypeDocument(int type)
+        {
+            Dictionary<string, string> test = new Dictionary<string, string>();
+            if (type == 1)
+            {
+                test.Add("N", "NIT");
+            }
+            else
+            {
+                test.Add("C", "Cédula de ciudadanía");
+                test.Add("E", "Cédula de extranjería");
+                test.Add("P", "Pasaporte");
+                test.Add("I", "Tarjeta de identidad");
+            }
+            CmbIdDType.DisplayMember = "Value";
+            CmbIdDType.ValueMember = "Key";
+        }
+
+        private void CmbIdDType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (CmbTypeBuyer.SelectedIndex == 0)
+                {
+                    lblLastName.Visible = true;
+                    lblPrimerApellido.Text = "Primer Apellido";
+                    lblPrimerNombre.Text = "Primer Nombre";
+                    lblSegundoNombre.Text = "Segundo Nombre";
+                    txtPrimerApellido.Visible = true;
+                    lblPrimerApellido.Visible = true;
+                    FillTipoDocumento(0);
+                    ddlTipoDocumento.SelectedIndex = 0;
+                }
+                else
+                {
+                    lblLastName.Visible = false;
+                    lblPrimerApellido.Visible = false;
+                    lblPrimerNombre.Text = "Razon Social";
+                    lblSegundoNombre.Text = "Dirección";
+                    txtPrimerApellido.Visible = false;
+                    FillTipoDocumento(1);
+                    ddlTipoDocumento.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                objUtil.Exception(ex.Message);
+            }
+        }
     }
 }
