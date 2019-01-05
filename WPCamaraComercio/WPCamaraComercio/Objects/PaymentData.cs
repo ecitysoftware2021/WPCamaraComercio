18576f6742d135dea3336c13c16941e2dfbf4dbe
dcordobag <32066181+dcordobag@users.noreply.github.com>
2019-01-05 14:02:50 -0500

    update

---
+++ WPCamaraComercio/WPCamaraComercio/Objects/PaymentData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCamaraComercio.Objects
+{
+    public class PaymentData
+    {
+        public string Data1 { get; set; }
+        public string Data2 { get; set; }
+        public string Data3 { get; set; }
+        public string Data4 { get; set; }
+    }
+}
--- WPCamaraComercio/WPCamaraComercio/Views/FrmPaymentData.xaml.cs
+++ WPCamaraComercio/WPCamaraComercio/Views/FrmPaymentData.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPCamaraComercio.Objects;
 
 namespace WPCamaraComercio.Views
 {
@@ -19,14 +20,11 @@ namespace WPCamaraComercio.Views
     /// </summary>
     public partial class FrmPaymentData : Window
     {
-        string data1 = string.Empty;
-        string data2 = string.Empty;
-        string data3 = string.Empty;
-        string data4 = string.Empty;
 
         public FrmPaymentData()
         {
             InitializeComponent();
+            
             CmbTypeBuyer.SelectedIndex = 0;
             CmbIdDType.SelectedIndex = 0;
         }
@@ -38,44 +36,59 @@ namespace WPCamaraComercio.Views
 
         public void FillTypeDocument(int type)
         {
-            Dictionary<string, string> test = new Dictionary<string, string>();
-            if (type == 1)
+            try
             {
-                test.Add("N", "NIT");
+                Dictionary<string, string> test = new Dictionary<string, string>();
+                if (type == 1)
+                {
+                    test.Add("N", "NIT");
+                }
+                else
+                {
+                    test.Add("C", "Cédula de ciudadanía");
+                    test.Add("E", "Cédula de extranjería");
+                    test.Add("P", "Pasaporte");
+                    test.Add("I", "Tarjeta de identidad");
+                }
+
+                CmbIdDType.ItemsSource = test;
             }
-            else
+            catch (Exception ex)
             {
-                test.Add("C", "Cédula de ciudadanía");
-                test.Add("E", "Cédula de extranjería");
-                test.Add("P", "Pasaporte");
-                test.Add("I", "Tarjeta de identidad");
+
             }
-            CmbIdDType.DisplayMemberPath = "Value";
-            CmbIdDType.SelectedValuePath = "Key";
         }
 
         private void CmbTypeBuyer_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
+                PaymentData data = new PaymentData();
+
                 if (CmbTypeBuyer.SelectedIndex == 0)
                 {
-                    data1 = "Primer Nombre";
-                    data2 = "Segundo Nombre";
-                    data3 = "Primer Apellido";
-                    data4 = "Teléfono";
+                    data.Data1 = "Primer Nombre";
+                    data.Data2 = "Segundo Nombre";
+                    data.Data3 = "Primer Apellido";
+                    data.Data4 = "Teléfono";
+                    TxbData4.Visibility = Visibility.Visible;
+                    TbxInfo4.Visibility = Visibility.Visible;
+
+                    grdPaymentData.DataContext = data;
 
                     FillTypeDocument(0);
                     CmbIdDType.SelectedIndex = 0;
                 }
                 else
                 {
-                    data1 = "Razón Social";
-                    data2 = "Dirección";
-                    data3 = "Teléfono";
-                    data4 = "";
+                    data.Data1 = "Razón Social";
+                    data.Data2 = "Dirección";
+                    data.Data3 = "Teléfono";
+                    data.Data4 = "";
                     TxbData4.Visibility = Visibility.Hidden;
                     TbxInfo4.Visibility = Visibility.Hidden;
+
+                    grdPaymentData.DataContext = data;
 
                     FillTypeDocument(1);
                     CmbIdDType.SelectedIndex = 0;
