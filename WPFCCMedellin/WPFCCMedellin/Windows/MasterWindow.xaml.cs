8ea32e02d0f054a281b15c5d835d18fe96fef7bb
dcordobag <32066181+dcordobag@users.noreply.github.com>
2020-06-19 16:55:40 -0500

    ok

--- WPFCCMedellin/WPFCCMedellin/Windows/MasterWindow.xaml.cs
+++ WPFCCMedellin/WPFCCMedellin/Windows/MasterWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace WPFCCMedellin.Windows
         {
             try
             {
+                string a = Encryptor.Encrypt("usrapli");
+                string b = Encryptor.Encrypt("1Cero12019$/*");
+                string c = Encryptor.Encrypt("Pay+ CC Centro");
+                string d = Encryptor.Encrypt("CamaraCentro2020$");
+                string e = Encryptor.Encrypt("http://181.143.126.126:41400/");
+                string es = Encryptor.Encrypt("http://virtuales.camaramedellin.com.co/e-cer/");
+
                 WPKeyboard.Keyboard.ConsttrucKeyyboard(WPKeyboard.Keyboard.EStyle.style_1);
                 if (Utilities.navigator == null)
                 {
